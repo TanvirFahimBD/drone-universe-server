571a89445d1e6d1eb4c5eaea81e45415b671a54d
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-11-15 18:23:25 +0600

    up

---
+++ README.md
@@ -0,0 +1,2 @@
+# drone-universe-server-side
+# niche-website-server-side-TanvirFahimBD
---
+++ index.js
@@ -0,0 +1,170 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//CHANGED DIFFERENT APP
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hqjnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log("uri", uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("DB connected Successfully");
+    const database = client.db("droneUniverse");
+    const productsCollection = database.collection("products");
+    const reviewCollection = database.collection("reviews");
+    const orderCollection = database.collection("orders");
+    const userCollection = database.collection("users");
+
+    //products GET API
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // products specific GET API
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      console.log("id of order", id);
+      res.json(result);
+    });
+
+    //products POST API
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      console.log("req body", req.body);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    //products DELETE API
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      console.log("delete id", id);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    // Review GET API
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // Review POST API
+    app.post("/review", async (req, res) => {
+      const newReview = req.body;
+      const result = await reviewCollection.insertOne(newReview);
+      console.log("req body", req.body);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    //Orders POST API
+    app.post("/orders", async (req, res) => {
+      const newOrder = req.body;
+      const result = await orderCollection.insertOne(newOrder);
+      console.log("req body", req.body);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    //Orders Specific GET API
+    app.get("/orders", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      console.log(query);
+      const cursor = orderCollection.find(query);
+      const ordersUser = await cursor.toArray();
+      res.json(ordersUser);
+    });
+
+    // Orders GET API
+    app.get("/orders", async (req, res) => {
+      const cursor = orderCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    //orders DELETE API
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      console.log("delete id", id);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    //Orders UPDATE API
+    // app.put("/orders", async (req, res) => {
+    //   const user = req.body;
+    //   const filter = { email: user.email };
+    //   const updateDoc = { $set: { status: "shipped" } };
+    //   const result = await orderCollection.updateOne(filter, updateDoc);
+    //   res.json(result);
+    // });
+
+    //users POST API
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      console.log("req body", req.body);
+      console.log("result", result);
+      res.json(result);
+    });
+
+    //users UPDATE API
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    });
+
+    //admin UPDATE API
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      console.log("put", user);
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Drone Universe server");
+});
+
+app.listen(port, () => {
+  console.log("Running port ", port);
+});
